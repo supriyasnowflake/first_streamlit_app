3434525ca339dfc9598634eee86a3fe8d882c8e0
supriyasnowflake <123443817+supriyasnowflake@users.noreply.github.com>
2023-02-01 17:52:52 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+  return fruityvice_normalized;
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -38,9 +38,9 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 
-except URLError as e:
+#except URLError as e:
 
-#streamlit.header("The fruit load list contains:")
+streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
